18ade323d29007a660576c8104903d15867e2b5c
Richardbmk <rdobmk@gmail.com>
2022-01-17 23:14:26 +0100

    cart buttons finish

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,12 +1,12 @@
 import React from "react";
 import NavContainer from "./Navbar-style";
-import logo from "../assets/logo.svg";
+import logo from "../../assets/logo.svg";
 import { FaBars } from "react-icons/fa";
 import { Link } from "react-router-dom";
-import { links } from "../utils/constants";
-import CartButtons from "./CartButtons";
-import { useProductsContext } from "../context/products_context";
-import { useUserContext } from "../context/user_context";
+import { links } from "../../utils/constants";
+import CartButtons from "../cartButtons/CartButtons";
+import { useProductsContext } from "../../context/products_context";
+import { useUserContext } from "../../context/user_context";
 
 const Nav = () => {
   return (
@@ -16,7 +16,21 @@ const Nav = () => {
           <Link to="/">
             <img src={logo} alt="comfy sloth" />
           </Link>
+          <button type="button" className="nav-toggle">
+            <FaBars />
+          </button>
         </div>
+        <ul className="nav-links">
+          {links.map((link) => {
+            const { id, text, url } = link;
+            return (
+              <li key={id}>
+                <Link to={url}>{text}</Link>
+              </li>
+            );
+          })}
+        </ul>
+        <CartButtons />
       </div>
     </NavContainer>
   );
