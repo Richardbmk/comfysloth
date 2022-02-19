9502fabf63eb69c29509f642493588269a81cbc2
Richardbmk <rdobmk@gmail.com>
2022-02-19 22:50:53 +0100

    ProductImages component done

--- src/pages/singleProductPage/SingleProductPage.js
+++ src/pages/singleProductPage/SingleProductPage.js
@@ -16,6 +16,7 @@ import { Link } from 'react-router-dom';
 
 const SingleProductPage = () => {
   const { id } = useParams();
+  const history = useHistory();
   const {
     single_product_loading: loading,
     single_product_error: error,
@@ -27,9 +28,66 @@ const SingleProductPage = () => {
     fetchSingleProduct(`${url}${id}`);
   }, [id]);
 
-  console.log(product);
+  useEffect(() => {
+    if (error) {
+      setTimeout(() => {
+        history.push('/');
+      }, 3000);
+    }
+  }, [error]);
 
-  return <h4>single product page</h4>;
+  if (loading) {
+    return <Loading />;
+  }
+
+  if (error) {
+    return <Error />;
+  }
+
+  const {
+    name,
+    price,
+    description,
+    stock,
+    stars,
+    reviews,
+    id: sku,
+    company,
+    images,
+  } = product;
+
+  return (
+    <Wrapper>
+      <PageHero title={name} product />
+      <div className='section section-center page'>
+        <Link to='/products' className='btn'>
+          back to products
+        </Link>
+        <div className='product-center'>
+          <ProductImages images={images} />
+          <section className='content'>
+            <h2>{name}</h2>
+            <Stars />
+            <h5 className='price'>{formatPrice(price)}</h5>
+            <p className='desc'>{description}</p>
+            <p className='info'>
+              <span>Available: </span>
+              {stock > 0 ? 'In stock' : 'out of stock'}
+            </p>
+            <p className='info'>
+              <span>SKU: </span>
+              {sku}
+            </p>
+            <p className='info'>
+              <span>Available: </span>
+              {company}
+            </p>
+            {stock > 0 && <AddToCart />}
+          </section>
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default SingleProductPage;
