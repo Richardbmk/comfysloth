4d446143fbea998cd6b185f0da8ae7f89200c746
Richardbmk <rdobmk@gmail.com>
2022-02-28 13:57:28 +0100

    Cart Page Setup done

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import Home from './homePage/HomePage';
 import Products from './productsPage/ProductsPage';
 import SingleProduct from './singleProductPage/SingleProductPage';
 import About from './aboutPage/AboutPage';
-import Cart from './CartPage';
+import Cart from './cartPage/CartPage';
 import Checkout from './checkoutPage/CheckoutPage';
 import Error from './errorPage/ErrorPage';
 import PrivateRoute from './PrivateRoute';
