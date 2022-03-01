775997964d43ab3832be84e6b6d992b1ff7d40a8
Richardbmk <rdobmk@gmail.com>
2022-03-01 12:33:08 +0100

    Cart Totals done

--- src/components/carContent/CarContent.js
+++ src/components/carContent/CarContent.js
@@ -2,9 +2,9 @@ import React from 'react';
 import Wrapper from './carContent-style';
 import { useCartContext } from '../../context/cart_context';
 import { Link } from 'react-router-dom';
-import CartColumns from './../CartColumns';
+import CartColumns from './../cartColumns/CartColumns';
 import CartItem from './../CartItem';
-import CartTotals from './../CartTotals';
+import CartTotals from './../cartTotals/CartTotals';
 
 const CartContent = () => {
   const { cart, clearCart } = useCartContext();
