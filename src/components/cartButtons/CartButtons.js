e9abe298a9b36f3aef91a702faf20cbd9bc5d282
Richardbmk <rdobmk@gmail.com>
2022-03-02 07:34:57 +0100

    Cart - Calculate Totals done

--- src/components/cartButtons/CartButtons.js
+++ src/components/cartButtons/CartButtons.js
@@ -1,23 +1,24 @@
-import React from "react";
-import { FaShoppingCart, FaUserMinus, FaUserPlus } from "react-icons/fa";
-import { Link } from "react-router-dom";
-import Wrapper from "./cartButtons-style";
-import { useProductsContext } from "../../context/products_context";
-import { useCartContext } from "../../context/cart_context";
-import { useUserContext } from "../../context/user_context";
+import React from 'react';
+import { FaShoppingCart, FaUserMinus, FaUserPlus } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import Wrapper from './cartButtons-style';
+import { useProductsContext } from '../../context/products_context';
+import { useCartContext } from '../../context/cart_context';
+import { useUserContext } from '../../context/user_context';
 
 const CartButtons = () => {
   const { closeSidebar } = useProductsContext();
+  const { total_items } = useCartContext();
   return (
-    <Wrapper className="cart-btn-wrapper">
-      <Link to="/cart" className="cart-btn" onClick={closeSidebar}>
+    <Wrapper className='cart-btn-wrapper'>
+      <Link to='/cart' className='cart-btn' onClick={closeSidebar}>
         Cart
-        <span className="cart-container">
+        <span className='cart-container'>
           <FaShoppingCart />
-          <span className="cart-value">12</span>
+          <span className='cart-value'>{total_items}</span>
         </span>
       </Link>
-      <button type="button" className="auth-btn" onClick={closeSidebar}>
+      <button type='button' className='auth-btn' onClick={closeSidebar}>
         Login <FaUserPlus />
       </button>
     </Wrapper>
