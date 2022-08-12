dfb4eddd290754ca5f623b22fe146a98c7914e51
Richardbmk <rdobmk@gmail.com>
2022-08-12 18:35:19 +0200

    formspree subscribe added

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,24 +1,29 @@
-import React from "react";
-import Wrapper from "./contact-style";
+import React from 'react';
+import Wrapper from './contact-style';
 
 const Contact = () => {
   return (
     <Wrapper>
-      <div className="section-center">
+      <div className='section-center'>
         <h3>Join our newsletter and get 20% off</h3>
-        <div className="content">
+        <div className='content'>
           <p>
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus
             explicabo tempora incidunt accusamus soluta nulla officiis magnam
-            accusantium facere repudiandae?
+            accusantium facere repudiandae?{' '}
           </p>
-          <form action="" className="contact-form">
+          <form
+            action='https://formspree.io/f/myyvyjyy'
+            method='POST'
+            className='contact-form'
+          >
             <input
-              type="email"
-              className="form-input"
-              placeholder="enter email"
+              type='email'
+              name='email'
+              className='form-input'
+              placeholder='enter email'
             />
-            <button type="submit" className="submit-btn">
+            <button type='submit' className='submit-btn'>
               subscribe
             </button>
           </form>
