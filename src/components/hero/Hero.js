51c4e2bbf73776ecd132ab0e5bfce895e8a13618
Richardbmk <rdobmk@gmail.com>
2023-05-31 22:34:49 +0200

    new favicon added

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -1,15 +1,15 @@
-import React from "react";
-import Wrapper from "./hero-style";
-import { Link } from "react-router-dom";
-import heroBcg from "../../assets/hero-bcg.jpeg";
-import heroBcg2 from "../../assets/hero-bcg-2.jpeg";
+import React from 'react';
+import Wrapper from './hero-style';
+import { Link } from 'react-router-dom';
+import heroBcg from '../../assets/hero-bcg.jpeg';
+import heroBcg2 from '../../assets/hero-bcg-2.jpeg';
 
 const Hero = () => {
   return (
-    <Wrapper className="section-center">
-      <article className="content">
+    <Wrapper className='section-center'>
+      <article className='content'>
         <h1>
-          design your <br />
+          Make Home your <br />
           comfort zone
         </h1>
         <p>
@@ -17,13 +17,13 @@ const Hero = () => {
           debitis dolore atque corporis dolorem fugiat eum laborum quia quod
           dolores!
         </p>
-        <Link to="/products" className="btn hero-btn">
+        <Link to='/products' className='btn hero-btn'>
           shop now
         </Link>
       </article>
-      <article className="img-container">
-        <img src={heroBcg} alt="nice table" className="main-img" />
-        <img src={heroBcg2} alt="person working" className="accent-img" />
+      <article className='img-container'>
+        <img src={heroBcg} alt='nice table' className='main-img' />
+        <img src={heroBcg2} alt='person working' className='accent-img' />
       </article>
     </Wrapper>
   );
